9e0852d62e3f237f917c8b36196d93d801bea9cf
田知勲 <j-jihoon@winas.jp>
2021-02-22 23:31:00 +0900

    first commit

---
+++ TikTok/TikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Jh's Macbook Pro on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
